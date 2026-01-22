a77763af337fcbf3b22f528abe3292a0cbebeaa1
Jason_T. <118946259+Nite1222@users.noreply.github.com>
2026-01-22 09:46:16 -0800

    Update PigLatin.java

--- lib/src/main/java/org/APCSLowell/PigLatin.java
+++ lib/src/main/java/org/APCSLowell/PigLatin.java
@@ -20,7 +20,7 @@ public class PigLatin {
 		boolean allConsonants= true;
 		if(sWord.length()>0){
 			for(int i=0;i<sWord.length();i++){
-				if(sWord.substring(0,1).equals("a") || sWord.substring(0,1).equals("e") || sWord.substring(0,1).equals("i") || sWord.substring(0,1).equals("o") || sWord.substring(0,1).equals("u")){
+				if(sWord.substring(i,i+1).equals("a") || sWord.substring(i,i+1).equals("e") || sWord.substring(i,i+1).equals("i") || sWord.substring(i,i+1).equals("o") || sWord.substring(i,i+1).equals("u")){
 				allConsonants=false;
 				}
 			}
